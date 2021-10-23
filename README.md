96071ef1d82a4c4b76ef9774ea2a698765cd6f98
SamB133 <64711289+SamB133@users.noreply.github.com>
2021-10-23 06:18:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-App
+ A web app created using Svelte that allows a user to enter a city or zip code and outputs the weather information for that location using the OpenWeatherMap API.
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	let location;
+</script>
+
+<main>
+	<h1>Find local weather</h1>
+	<input placeholder="Enter City or Zip" bind:value={location} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: blue;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 300;
+	}
+
+	input {
+		text-align: center;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
