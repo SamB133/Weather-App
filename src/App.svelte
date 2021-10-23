e6512bcebdf6e966c1c15b4b2882e1f2bc2dae27
SamB133 <64711289+SamB133@users.noreply.github.com>
2021-10-23 07:17:37 -0500

    Able to send user input to a function

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
 	let location;
+
+	function searchLocation(location) {
+		location;
+	}
 </script>
 
 <main>
 	<h1>Find local weather</h1>
-	<input placeholder="Enter City or Zip" bind:value={location} />
+	<input
+		on:keypress={(e) => {
+			if (e.key === "Enter") searchLocation(location);
+		}}
+		placeholder="Enter City or Zip"
+		bind:value={location}
+	/>
+	<button on:click={searchLocation(location)}>Search</button>
 </main>
 
 <style>
