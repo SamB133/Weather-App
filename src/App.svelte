bc6c47f4ad4bf01a5be3ce54b24c5057f3d1e65b
SamB133 <64711289+SamB133@users.noreply.github.com>
2021-10-23 22:26:00 -0500

    Moved all CSS together
    Moved the CSS stylings from App.svelte to global.css.

--- src/App.svelte
+++ src/App.svelte
@@ -17,29 +17,3 @@
 	/>
 	<button on:click={searchLocation(location)}>Search</button>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: blue;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 300;
-	}
-
-	input {
-		text-align: center;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
